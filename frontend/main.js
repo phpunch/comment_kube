1a16062a2b963a834ce6896640d5e7a06fc6e4af
iamsirid <iamsirid@gmail.com>
2019-11-23 15:30:13 +0700

    add frontend

---
+++ frontend/main.js
@@ -0,0 +1,56 @@
+const updateCommentList = commentList => {
+  const boxes = document.querySelector(".boxes");
+  boxes.innerHTML = "";
+  commentList.forEach(comment => {
+    const box = document.createElement("div");
+    box.className = "box";
+
+    box.innerHTML = `<div class="name">${comment.name}</div><div class="msg"><p>${comment.msg}</p></div>`;
+    boxes.appendChild(box);
+  });
+
+  console.log(commentList);
+};
+
+// fetch("data.json")
+fetch("http://backend:5000/api/post")
+  .then(response => {
+    if (response.ok) {
+      return response.json();
+    }
+  })
+  .then(({ commentList }) => {
+    updateCommentList(commentList);
+  })
+  .catch(err => console.error(err));
+
+const submitBtn = document.querySelector(".submit-btn");
+submitBtn.addEventListener("click", () => {
+  const msgTextArea = document.querySelector("textarea[name=msg]");
+  const nameInput = document.querySelector("input[name=name]");
+  const msg = msgTextArea.value;
+  const name = nameInput.value;
+  msgTextArea.value = "";
+  nameInput.value = "";
+
+  const comment = {
+    name: name,
+    msg: msg
+  };
+
+  const options = {
+    method: "POST",
+    body: JSON.stringify(comment),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  };
+
+  fetch("http://backend:5000/api/post", options)
+    .then(res => res.json())
+    .then(({ commentList }) => updateCommentList(commentList));
+
+  //   fetch("data2.json")
+  //     .then(res => res.json())
+  //     .then(({ commentList }) => updateCommentList(commentList));
+});
