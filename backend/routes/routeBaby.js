704cd9c26bb1493e8409157ae8eafe789e5d454a
Saranpat Prasertthum <Saranpatp@gmail.com>
2019-11-23 17:53:32 +0700

    fix delete route

--- backend/routes/routeBaby.js
+++ backend/routes/routeBaby.js
@@ -2,29 +2,31 @@ const mongoose = require("mongoose");
 const Comment = mongoose.model("comment");
 
 module.exports = app => {
-  const getAll= () => {
+  const getAll = async () => {
     const comments = await Comment.find({});
-    return comments
-  }
+    return comments;
+  };
   app.post("/api/post", async (req, res) => {
     const { name, msg } = req.body;
     await new Comment({
       name,
       msg
     }).save();
-
-    res.send(getAll());
+    const comments = await getAll();
+    res.send(comments);
 
     // res.status(200);
   });
   app.get("/api/get", async (req, res) => {
-    res.send(getAll());
+    const comments = await getAll();
+    res.send(comments);
   });
   app.delete("/api/delete/:id", async (req, res) => {
     try {
-      Comment.deleteOne({ _id: id }, function(err) {
+      Comment.deleteOne({ _id: req.params.id }, async function(err) {
         if (err) throw err;
-        res.send(getAll());
+        const comments = await getAll();
+        res.send(comments);
       });
     } catch (err) {
       console.log(err);
