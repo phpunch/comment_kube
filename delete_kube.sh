42775f208f8146b5dede0cfc0750cc563c164129
phpunch <nutacp_ryho@hotmail.com>
2019-11-30 20:42:05 +0700

    Fix make_kube and delete_kube

--- delete_kube.sh
+++ delete_kube.sh
@@ -1,3 +1,5 @@
 kubectl delete -f deployment-db.yaml
 kubectl delete -f deployment-backend.yaml
+kubectl delete -f deployment-backend2.yaml
+kubectl delete -f deployment-logging.yaml
 kubectl delete -f deployment-frontend.yaml
--- make_kube.sh
+++ make_kube.sh
@@ -1,3 +1,5 @@
 kubectl apply -f deployment-db.yaml
 kubectl apply -f deployment-backend.yaml
+kubectl apply -f deployment-backend2.yaml
+kubectl apply -f deployment-logging.yaml
 kubectl apply -f deployment-frontend.yaml
