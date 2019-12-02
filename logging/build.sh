2feba4d6a0d80b4a821b642b500fa612ba460355
phpunch <nutacp_ryho@hotmail.com>
2019-12-02 16:20:24 +0700

    Change docker hub repo

--- logging/build.sh
+++ logging/build.sh
@@ -1,2 +1,2 @@
-docker buildx build -t saranpat/comment_kube_logging .
-docker push saranpat/comment_kube_logging
+docker buildx build -t phpunch/comment_kube_logging .
+docker push phpunch/comment_kube_logging
